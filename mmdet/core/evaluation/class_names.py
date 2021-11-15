2190959da762a26559e47946e45f1e517c803998
ange33333333 <72069714+ange33333333@users.noreply.github.com>
2021-11-15 17:53:56 +0800

    Add files via upload

--- mmdet/core/evaluation/class_names.py
+++ mmdet/core/evaluation/class_names.py
@@ -8,9 +8,7 @@ def wider_face_classes():
 
 def voc_classes():
     return [
-        'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat',
-        'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person',
-        'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'
+        '10','1','2','3','4','5','6','7','8','9'
     ]
 
 
