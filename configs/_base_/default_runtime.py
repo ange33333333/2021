ded8c9063658ff763bdd3f8540aef82ef55850b6
ange33333333 <72069714+ange33333333@users.noreply.github.com>
2021-11-25 18:36:53 +0800

    Update default_runtime.py

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -12,6 +12,6 @@ custom_hooks = [dict(type="NumClassCheckHook")]
 
 dist_params = dict(backend="nccl")
 log_level = "INFO"
-load_from = "https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_mstrain_3x_coco/faster_rcnn_r50_fpn_mstrain_3x_coco_20210524_110822-e10bd31c.pth"
+load_from = "https://download.openmmlab.com/mmdetection/v2.0/cascade_rcnn/cascade_mask_rcnn_r50_fpn_mstrain_3x_coco/cascade_mask_rcnn_r50_fpn_mstrain_3x_coco_20210628_164719-5bdc3824.pth"
 resume_from = None
 workflow = [("train", 2), ("val", 1)]
