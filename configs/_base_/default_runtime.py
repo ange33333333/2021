b4f746320cc23a37b9c37bad6b477173a4004aab
ange33333333 <72069714+ange33333333@users.noreply.github.com>
2021-11-25 22:13:13 +0800

    Update default_runtime.py

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -14,4 +14,4 @@ dist_params = dict(backend="nccl")
 log_level = "INFO"
 load_from = "https://download.openmmlab.com/mmdetection/v2.0/cascade_rcnn/cascade_mask_rcnn_r50_fpn_mstrain_3x_coco/cascade_mask_rcnn_r50_fpn_mstrain_3x_coco_20210628_164719-5bdc3824.pth"
 resume_from = None
-workflow = [("train", 2), ("val", 1)]
+workflow = [("train", 1), ("val", 1)]
