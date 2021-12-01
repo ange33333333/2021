48152b6a60866754f96c4609b2bc411792612e25
ange33333333 <72069714+ange33333333@users.noreply.github.com>
2021-12-02 02:58:29 +0800

    Add files via upload

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -1,7 +1,7 @@
 checkpoint_config = dict(interval=1)
 # yapf:disable
 log_config = dict(
-    interval=50,
+    interval=10,
     hooks=[
         dict(type="TextLoggerHook"),
         # dict(type='TensorboardLoggerHook')
@@ -12,6 +12,6 @@ custom_hooks = [dict(type="NumClassCheckHook")]
 
 dist_params = dict(backend="nccl")
 log_level = "INFO"
-load_from = "https://download.openmmlab.com/mmdetection/v2.0/cascade_rcnn/cascade_mask_rcnn_r50_fpn_mstrain_3x_coco/cascade_mask_rcnn_r50_fpn_mstrain_3x_coco_20210628_164719-5bdc3824.pth"
+load_from = "https://download.openmmlab.com/mmdetection/v2.0/mask_rcnn/mask_rcnn_r50_fpn_mstrain-poly_3x_coco/mask_rcnn_r50_fpn_mstrain-poly_3x_coco_20210524_201154-21b550bb.pth"
 resume_from = None
 workflow = [("train", 1), ("val", 1)]
