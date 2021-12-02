70c59d4fb972e06b4d30b90f3bd46150f1afefd0
ange33333333 <72069714+ange33333333@users.noreply.github.com>
2021-12-02 23:56:02 +0800

    Add files via upload

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -12,6 +12,6 @@ custom_hooks = [dict(type="NumClassCheckHook")]
 
 dist_params = dict(backend="nccl")
 log_level = "INFO"
-load_from = "https://download.openmmlab.com/mmdetection/v2.0/mask_rcnn/mask_rcnn_r50_fpn_mstrain-poly_3x_coco/mask_rcnn_r50_fpn_mstrain-poly_3x_coco_20210524_201154-21b550bb.pth"
+load_from = "https://download.openmmlab.com/mmdetection/v2.0/mask_rcnn/mask_rcnn_r101_fpn_mstrain-poly_3x_coco/mask_rcnn_r101_fpn_mstrain-poly_3x_coco_20210524_200244-5675c317.pth"
 resume_from = None
 workflow = [("train", 1), ("val", 1)]
