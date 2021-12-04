72f915e7af54d0a993c54d9469beb57a87345d9c
ange33333333 <72069714+ange33333333@users.noreply.github.com>
2021-12-05 02:55:02 +0800

    Update schedule_1x.py

--- configs/_base_/schedules/schedule_1x.py
+++ configs/_base_/schedules/schedule_1x.py
@@ -3,6 +3,6 @@ optimizer = dict(type="SGD", lr=0.0025, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
-    policy="step", warmup="linear", warmup_iters=500, warmup_ratio=0.001, step=[50,80]
+    policy="step", warmup="linear", warmup_iters=500, warmup_ratio=0.001, step=[60,110,150,180]
 )
-runner = dict(type="EpochBasedRunner", max_epochs= 100)
+runner = dict(type="EpochBasedRunner", max_epochs= 200)
